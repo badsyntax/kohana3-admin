5c0e5adcc23aa76c85bf09e0f7c99295a740ec0a
Richard Willis <willis.rh@gmail.com>
2010-12-04 10:24:57 +0000

    moved directories logic out of view

--- views/admin/page/logs.php
+++ views/admin/page/logs.php
@@ -1,27 +1,7 @@
-<ol class="logs-folders">
-<?php foreach($logs as $year => $months){?>
-<li>
-	<?php echo HTML::anchor('#year', str_replace('logs/', '', $year))?>
-	<?php if (is_array($months)){?>
-	<ol>
-		<?php foreach($months as $month => $day){?>
-		<li>
-			<?php echo HTML::anchor('#month', date('F', mktime(0, 0, 0, (int) str_replace($year.'/', '', $month), 1, date('Y'))))?>
-			<?php if (is_array($day)){?>
-			<ol>
-				<?php foreach($day as $log){?>
-				<li>
-					<?php echo HTML::anchor('admin/'.$month.'/'.basename($log), preg_replace('/.*?(\d+)'.EXT.'$/', '$1', $log))?>
-				</li>
-				<?php }?>
-			</ol>
-			<?php }?>
-		</li>
-		<?php }?>
-	</ol>
-	<?php }?>
-</li>
-<?php }?>
+<div class="logs-directories">
+<?php echo $directories ?>
+</div>
+
 <script type="text/javascript">
 (function($){
 	$('a[href="#year"], a[href="#month"]').click(function(){
@@ -32,3 +12,11 @@
 	});
 })(this.jQuery);
 </script>
+
+<?php if ($entries !== NULL){?>
+<div class="logs-entries">
+	<?php foreach($entries as $entry){?>
+		<div><?php echo $entry?></div>
+	<?php }?>
+</div>
+<?php }?>
