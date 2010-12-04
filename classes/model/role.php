20373191161ed562e20e97fdefe39ffc7e8b1a33
Richard Willis <willis.rh@gmail.com>
2010-12-04 01:48:06 +0000

    added admin role model

---
+++ classes/model/role.php
@@ -0,0 +1,33 @@
+<?php
+
+class Model_Role extends Model_Base_Role {
+
+        public function admin_create(& $data)
+        {
+                $data = Validate::factory($data)
+                        ->rules('name', $this->_rules['name'])
+                        ->rules('description', $this->_rules['description']);
+
+                if (!$data->check()) return FALSE;
+
+                $this->values($data);
+                $this->save();
+
+                return $data;
+        }
+
+        public function admin_update(& $data)
+        {
+                $data = Validate::factory($data)
+                        ->rules('name', $this->_rules['name'])
+                        ->rules('description', $this->_rules['description']);
+
+                if (!$data->check()) return FALSE;
+
+                $this->values($data);
+                $this->save();
+
+                return $data;
+        }
+	
+} // End Model_Role
