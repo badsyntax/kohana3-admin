389868918a2aace9f3b6cd772d3c8b15c0a5532f
Richard Willis <willis.rh@gmail.com>
2010-12-04 11:02:29 +0000

    simplified the directories builder

--- classes/controller/admin/logs.php
+++ classes/controller/admin/logs.php
@@ -7,16 +7,17 @@ class Controller_Admin_Logs extends Controller_Admin_Base {
 		$this->template->title = __('Admin - Logs');
 		$this->template->content = View::factory('admin/page/logs')
 			->bind('directories', $directories)
-			->bind('entries', $entries);
+			->bind('entries', $entries)
+			->bind('total_files', $total_files);
 
-		$cur_month =
-		$cur_year = NULL;
+		$cur_month = $cur_year = NULL;
+		$total_files = 0;
 
 		$entries = $this->get_entries($file, $cur_year, $cur_month);
 
 		$logs = Kohana::list_files('logs');
 
-		$directories = $this->get_directories_html($logs, $cur_year, $cur_month);
+		$directories = $this->get_directories_html($logs, $cur_year, $cur_month, $total_files);
 	}
 
 	private function get_entries(& $file = NULL, & $cur_year = NULL, & $cur_month = NULL, & $cur_day = NULL)
@@ -45,45 +46,34 @@ class Controller_Admin_Logs extends Controller_Admin_Base {
 		return $entries;
 	}
 
-	private function get_directories_html($logs, $cur_year = NULL, $cur_month = NULL)
+	private function get_directories_html($logs, $cur_year = NULL, $cur_month = NULL, & $total_files = 0)
 	{
 		$html = '<ol>';
 		foreach($logs as $year => $months){
-
+			
 			$year_trim = str_replace('logs/', '', $year); 
 			$attributes = ($year_trim == $cur_year) ? array('class' => 'selected open') : NULL; 
 
-			$html .= HTML::anchor('#year', $year_trim, $attributes);
+			$html .= '<li>' .HTML::anchor('#year', $year_trim, $attributes) . '<ol'.HTML::attributes($attributes).'>';
 
-			if (is_array($months)){
+			foreach($months as $month => $day){
 
-				$html .= '<ol'.HTML::attributes($attributes).'>';
-				foreach($months as $month => $day){
+				$month_trim = str_replace("logs/{$year_trim}/", '', $month);
+				$month_name = date('F', mktime(0, 0, 0, $month_trim, 1, date('Y')));
+				$attributes = ($month_trim == $cur_month) ? array('class' => 'selected open') : NULL;
 
-					$month_trim = str_replace("logs/{$year_trim}/", '', $month);
-					$month_name = date('F', mktime(0, 0, 0, (int) str_replace($year.'/', '', $month_trim), 1, date('Y')));
-					$attributes = ($month_trim == $cur_month) ? array('class' => 'selected open') : NULL;
+				$html .= '<li>' . HTML::anchor('#month', $month_name, $attributes) . '<ol'.HTML::attributes($attributes).'>';
 
+				foreach($day as $log){
+					$total_files += 1;
 					$html .= '<li>';
-
-					$html .= HTML::anchor('#month', $month_name, $attributes);
-
-					if (is_array($day)){
-
-						$html .= '<ol'.HTML::attributes($attributes).'>';
-						foreach($day as $log){
-							$html .= '<li>';
-							$html .= HTML::anchor('admin/'.$month.'/'.basename($log), preg_replace('/.*?(\d+)'.EXT.'$/', '$1', $log));
-							$html .= '</li>';
-						}
-						$html .= '</ol>';
-					}
+					$html .= HTML::anchor('admin/'.$month.'/'.basename($log), preg_replace('/.*?(\d+)'.EXT.'$/', '$1', $log));
 					$html .= '</li>';
 				}
-				$html .= '</ol>';
+				$html .= '</ol></li>';
 			}
-			$html .= '</li>';
-		}
+			$html .= '</ol></li>';
+		} 
 		$html .= '</ol>';
 
 		return $html;
