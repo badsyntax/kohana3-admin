145e92c43dcbf3766aabb75328526f5558f44372
Richard Willis <willis.rh@gmail.com>
2010-12-05 14:50:35 +0000

    added logs routes

--- init.php
+++ init.php
@@ -1,14 +1,25 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * Admin routes 
+ */
 
-/* Admin routes */
+// Admin home
 Route::set('admin-home', 'admin')
 	->defaults(array(
 		'directory' => 'admin',
 		'controller' => 'home',
 		'action' => 'index'
 	));
+// Admin Actions
 Route::set('admin', 'admin/<controller>(/<action>)(/<id>)')
 	->defaults(array(
 		'action' => 'index',
 		'directory' => 'admin'
+	));
+// Logs
+Route::set('admin-logs', 'admin/logs(/<file>)', array('file' => '.+'))
+	->defaults(array(
+		'controller' => 'admin_logs',
+		'action'     => 'index',
+		'file'	     => NULL
 	));
