5250f79d23b64917698ae226aa3699b72691a3e2
Richard Willis <willis.rh@gmail.com>
2010-12-05 14:50:02 +0000

    added logs to view

--- views/admin/page/logs.php
+++ views/admin/page/logs.php
@@ -1,30 +1,38 @@
-<h1>Logs</h1>
-<div class="logs-directories">
-<?php echo $directories ?>
+<div class="action-bar clear">
+	<?php echo HTML::anchor('admin/logs/download/tar', 'Download logs', array('class' => 'button small helper-right'))?>
+	<h1>Logs</h1>
 </div>
 
-<script type="text/javascript">
-(function($){
-	$('a[href="#year"], a[href="#month"]').click(function(){
+<div class="clear">
+	<div class="logs-directories">
+	<?php echo $directories ?>
+	</div>
 
-		$(this).next().fadeToggle('fast');
+	<script type="text/javascript">
+	(function($){
+		$('a[href="#year"], a[href="#month"]').click(function(){
 
-		return false;
-	});
-})(this.jQuery);
-</script>
+			$(this).next().fadeToggle('fast');
 
-<div class="logs-entries">
-	<?php if ($entries !== NULL){?>
-		<p>
-			<strong><?php echo count($entries)?></strong> entries
-		</p>
-		<?php foreach($entries as $entry){?>
-			<div><?php echo $entry?></div>
+			return false;
+		});
+	})(this.jQuery);
+	</script>
+
+	<div class="logs-entries">
+		<?php if ($entries !== NULL){?>
+			<p>
+				<strong><?php echo count($entries)?></strong> entries
+			</p>
+			<?php foreach($entries as $entry){?>
+				<div id="<?php echo $entry['timestamp']?>">
+					<?php echo $entry['date'].' : '.$entry['log']?>
+				</div>
+			<?php }?>
+		<?php } else {?>
+			<p>
+				There <?php echo $total_files?> log files. Select a directory on the left to navigate the logs.
+			</p>
 		<?php }?>
-	<?php } else {?>
-		<p>
-			There <?php echo $total_files?> log files. Select a directory on the left to navigate the logs.
-		</p>
-	<?php }?>
+	</div>
 </div>
