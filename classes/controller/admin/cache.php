98d241d34e3370f479085c1235f95390df6d0d99
Richard Willis <willis.rh@gmail.com>
2010-12-10 22:42:03 +0000

    added cache controller

---
+++ classes/controller/admin/cache.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Admin_Cache extends Controller_Admin_Base {
+
+	public function action_index()
+	{
+		$this->template->title = __('Admin - Cache');
+		$this->template->content = View::factory('admin/page/cache')
+			->bind('cache_dir', $cache_dir)
+			->bind('total_size', $total_size)
+			->bind('total_files', $total_files);
+
+		$cache_dir = Kohana::$cache_dir;
+
+		$total_size = Text::bytes( (int) `du -sb {$cache_dir} | sed 's/\s.*$//g'`);
+
+		$total_files = `find {$cache_dir} -type f | wc -l`;
+	}
+
+	public function action_purge()
+	{
+		Cache::instance()->delete_all();
+
+		Activity::set(Activity::SUCCESS, __('Cache deleted.'));
+		Message::set(Message::SUCCESS, __('Cache successfully deleted.'));
+
+		$this->request->redirect('admin/cache');
+	}
+
+} // End Controller_Admin_Cache
