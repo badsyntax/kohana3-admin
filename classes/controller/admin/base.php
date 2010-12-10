e5e6fe6bc81b334c543b7c77ed307d498c34e600
Richard Willis <willis.rh@gmail.com>
2010-12-10 22:37:49 +0000

    added crud_model_singular attribute

--- classes/controller/admin/base.php
+++ classes/controller/admin/base.php
@@ -2,7 +2,7 @@
   
 abstract class Controller_Admin_Base extends Controller_Base {
  
- 	// Set the admin master page
+	// Set the admin master page
 	public $template = 'admin/page/master_page';
 
 	// Set the auto-crud model (optional)
@@ -19,6 +19,8 @@ abstract class Controller_Admin_Base extends Controller_Base {
 	{
 		// If the crud model isn't set then use the controller name (default)
 		$this->crud_model === FALSE AND $this->crud_model = $this->request->controller;
+		
+		$this->crud_model_singular = Inflector::singular($this->crud_model);
 
 		parent::before();
 	}
@@ -29,40 +31,40 @@ abstract class Controller_Admin_Base extends Controller_Base {
 		// Crud model needs to be set
 		$this->crud_model === FALSE AND $this->request->redirect('admin');
 
-                $this->template->title = __('Edit '.ucfirst($this->crud_model));
+		$this->template->title = __('Edit '.ucfirst($this->crud_model));
 
 		// Bind useful data objects to the view
-                $this->template->content = View::factory('admin/page/'.$this->crud_model)
-                        ->bind($this->crud_model, $items)
-                        ->bind('total', $total)
-                        ->bind('page_links', $page_links);
+		$this->template->content = View::factory('admin/page/'.$this->crud_model)
+			->bind($this->crud_model, $items)
+			->bind('total', $total)
+			->bind('page_links', $page_links);
 
 		// Get the total amount of items in the table
-                $total = ORM::factory( inflector::singular($this->crud_model) )->count_all();
+		$total = ORM::factory( $this->crud_model_singular )->count_all();
 
 		// Generate the pagination values
-                $pagination = Pagination::factory(array(
-                        'total_items' => $total,
-                        'items_per_page' => $this->pagination_items_per_page
-                ));
+		$pagination = Pagination::factory(array(
+				'total_items' => $total,
+				'items_per_page' => $this->pagination_items_per_page
+		));
 
 		// Get the items
-                $items = ORM::factory( inflector::singular($this->crud_model) )
-                        ->limit($pagination->items_per_page)
-                        ->offset($pagination->offset)
-                        ->find_all();
+		$items = ORM::factory( $this->crud_model_singular )
+				->limit($pagination->items_per_page)
+				->offset($pagination->offset)
+				->find_all();
 
 		// Generate the pagination links
-                $page_links = $pagination->render();
+		$page_links = $pagination->render();
 	}
 
 	public function authenticate()
 	{
-		// The user may be logged in but not have the correct permissions to view this controller and/or action, so 
-		// instead of redirecting to signin page we redirect to 403 Forbidden
+		// The user may be logged in but not have the correct permissions to view this controller and/or action, 
+		// so instead of redirecting to signin page we redirect to 403 Forbidden
 		Auth::instance()->logged_in() 
-		AND Auth::instance()->logged_in($this->auth_required) === FALSE
-		AND $this->request->redirect('403');
+			AND Auth::instance()->logged_in($this->auth_required) === FALSE
+			AND $this->request->redirect('403');
 
 		parent::authenticate();
 	}
@@ -70,13 +72,16 @@ abstract class Controller_Admin_Base extends Controller_Base {
 	// A generic delete action to deletes a model item by ID
 	public function action_delete($id = 0)
 	{
-		$item = ORM::factory(inflector::singular($this->crud_model), (int) $id);
+		$item = ORM::factory( $this->crud_model_singular, (int) $id);
 
 		! $item->loaded() AND $this->request->redirect('admin');
 
 		$item->delete();
 
-		Message::set(Message::SUCCESS, ucfirst(inflector::singular($this->crud_model)).' '.__('successfully deleted.'));
+		$message = ucfirst($this->crud_model_singular).' '.__('successfully deleted.');
+		
+		Activity::set(Activity::SUCCESS, $message);
+		Message::set(Message::SUCCESS, $message);
 
 		$this->request->redirect('admin/'.$this->crud_model);
 	}
