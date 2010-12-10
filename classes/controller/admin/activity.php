b962512507edeb0dbb25a69446066e10510aec31
Richard Willis <willis.rh@gmail.com>
2010-12-10 22:41:40 +0000

    added activity controller

---
+++ classes/controller/admin/activity.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Admin_Activity extends Controller_Admin_Base {
+
+	public function action_index()
+	{
+		$this->template->title = __('Admin - Activity');
+		$this->template->content = View::factory('admin/page/activity')
+			->bind('activities', $activities);
+
+		$activities = ORM::factory('activity')
+			->order_by('date', 'desc')
+			->find_all();
+	}
+
+} // End Controller_Admin_Notifications
