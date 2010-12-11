56d8949cbe9792f41d7ba78751a4490e464d4f21
Richard Willis <willis.rh@gmail.com>
2010-12-11 12:52:44 +0000

    added 'add page' button

--- views/admin/page/pages.php
+++ views/admin/page/pages.php
@@ -1,1 +1,9 @@
-<h1>Edit pages</h1>
+<div class="action-bar clear">
+	<a href="<?php echo URL::site('admin/pages/add')?>" class="button add small helper-right">
+		<span>Add page</span>
+	</a>
+
+	<h1>Pages</h1>
+</div>
+
+<?php echo $page_tree?>
