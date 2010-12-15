b7036c932e3646f65c1ab56ada81b36965955ed7
Richard Willis <willis.rh@gmail.com>
2010-12-15 00:56:26 +0000

    added callback validation functions for checking page id or parent id when updating or deleting a page

--- classes/model/page.php
+++ classes/model/page.php
@@ -27,14 +27,45 @@ class Model_Page extends Model_Base_Page {
 			->rules('title', $this->_rules['title'])
 			->rules('description', $this->_rules['description'])
 			->rules('uri', $this->_rules['uri'])
-			->rules('body', $this->_rules['body']);
+			->rules('body', $this->_rules['body'])
+			->callback('parent_id', array($this, 'check_parent_id'));
 		
 		if ( !$data->check()) return FALSE;
-
+		
 		$this->values($data);
 		$this->save();
 				
 		return $data;
 	}
 	
+	public function check_parent_id(Validate $array, $field)
+	{
+		if ( ! (bool) $this->parent_id )
+		{
+			$array->error($field, 'root_reparent', array($array[$field]));
+		}
+	}
+	
+	// Don't delete id 1
+	public function check_id(Validate $array, $field)
+	{
+		if ( (int) $this->id === 1)
+		{
+			$array->error($field, 'delete_id_1', array($array[$field]));
+		}
+	}
+	
+	public function delete_admin($id = NULL, & $data)
+	{
+		if ($id === NULL)
+		{
+			$data = Validate::factory($data)
+				->callback('id', array($this, 'check_id'));
+				
+			if ( !$data->check()) return FALSE;
+			
+		}
+		
+		return parent::delete($id);		
+	}
 } // End Model_User
