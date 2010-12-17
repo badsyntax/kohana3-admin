e8372f9e9aedf359fac9f8ddc1d36b80c5f8bce3
Richard Willis <willis.rh@gmail.com>
2010-12-17 20:03:52 +0000

    moved wysiwyg init into view

--- views/admin/media/js/wysiwyg.init.js.php
+++ views/admin/media/js/wysiwyg.init.js.php
@@ -6,16 +6,20 @@
 		mode : "textareas",
 		theme : "advanced",
 		skin : "cirkuit",
-		plugins : "safari,pagebreak,advimage,advlist,iespell,media,contextmenu,paste,nonbreaking,xhtmlxtras,jqueryinlinepopups",
+		debug : true,
+		plugins : '<?php echo Kohana::config('tinymce.plugins')?>',
+		
 		// Theme options
-		theme_advanced_buttons1 : "formatselect,|,bold,italic,strikethrough,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,|,link,unlink,|,image,media,|,code", 
+		theme_advanced_buttons1 : '<?php echo Kohana::config('tinymce.toolbar1')?>', 
 		theme_advanced_buttons2 : "",
 		theme_advanced_buttons3 : "",
 		theme_advanced_buttons4 : "",
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true
+		theme_advanced_resizing : true,
+		file_browser_callback : '<?php echo URL::site(TRUE, TRUE)?>'
+		
 	});
 
 })(this.jQuery, this);
