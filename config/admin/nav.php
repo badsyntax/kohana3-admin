4636cd21bbea84e70787cb97c2c84e5dc4e2aafa
Richard Willis <willis.rh@gmail.com>
2010-12-18 17:46:48 +0000

    added asset to nav

--- config/admin/nav.php
+++ config/admin/nav.php
@@ -6,6 +6,7 @@ return array(
 		'admin/pages' 		=> __('Pages'),
 		'admin/users' 		=> __('Users'),
 		'admin/roles' 		=> __('Roles'),
+		'admin/assets' 		=> __('Assets'),
 		'admin/logs' 		=> __('Logs'),
 		'admin/config' 		=> __('Config'),
 		'admin/cache' 		=> __('Cache'),
