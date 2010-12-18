b69df1484bb849f81af4c7e0d105f1959051ffb2
Richard Willis <willis.rh@gmail.com>
2010-12-18 17:46:25 +0000

    added get_asset assets routes

--- init.php
+++ init.php
@@ -10,12 +10,21 @@ Route::set('admin/media', 'admin/media(/<file>)', array('file' => '.+'))
 		'directory'	=> 'admin',
 		'file'       => NULL,
 	));
+	
+// Assets
+Route::set('admin-assets', 'admin/assets/get_asset(/<id>)(/<width>)(/<height>)(/<crop>)')
+	->defaults(array(
+		'action' => 'get_asset',
+		'directory' => 'admin',
+		'controller' => 'assets'
+	));
 // Admin Actions
 Route::set('admin', 'admin/<controller>(/<action>)(/<id>)')
 	->defaults(array(
 		'action' => 'index',
 		'directory' => 'admin'
 	));
+	
 // Logs
 Route::set('admin-logs', 'admin/logs(/<file>)', array('file' => '.+'))
 	->defaults(array(
@@ -23,6 +32,8 @@ Route::set('admin-logs', 'admin/logs(/<file>)', array('file' => '.+'))
 		'action'     => 'index',	
 		'file'	     => NULL
 	));
+	
+	
 // Admin home
 Route::set('admin-home', 'admin')
 	->defaults(array(
