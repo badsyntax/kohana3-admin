69026ec52a51f7d711c02d3c50bff12f3b9718b1
Richard Willis <willis.rh@gmail.com>
2010-12-18 17:42:38 +0000

    added get_asset method to dynamically resize and save assets

--- classes/controller/admin/assets.php
+++ classes/controller/admin/assets.php
@@ -26,11 +26,12 @@ class Controller_Admin_Assets extends Controller_Admin_Base {
 		$this->template->content = View::factory('admin/page/assets/upload')
 			->bind('errors', $errors);
 			
-		if (ORM::factory('asset')->upload_admin('asset', $_POST, $_FILES))
+		$upload_path = Kohana::config('admin/asset.upload_path');
+			
+		if ($assets = ORM::factory('asset')->upload_admin('asset', $upload_path, $_POST, $_FILES))
 		{
-			Message::set(Message::SUCCESS, __('Asset successfully saved.'));
-
-			$this->request->redirect('admin/assets');
+			Message::set(Message::SUCCESS, __('Asset'.(count($assets)?'s':'').' successfully saved.'));
+			$this->request->redirect('admin/assets/edit/'.implode(',', $assets));
 		}
 	
 		$errors = array_merge($_POST->errors('assets'), $_FILES->errors('assets'));
@@ -43,6 +44,38 @@ class Controller_Admin_Assets extends Controller_Admin_Base {
 		$_POST = $_POST->as_array();			
 	}
 	
+	public function action_get_asset($id = 0, $width = NULL, $height = NULL, $crop = FALSE)
+	{
+		$this->auto_render = FALSE;
+		
+		$asset = ORM::factory('asset', (int) $id);
+		
+		if (!$asset->loaded()) exit;
+		
+		$file = DOCROOT . Kohana::config('admin/asset.upload_path') .'/'. $asset->filename;
+		
+		$this->request->headers['Content-Type'] = $asset->mimetype;
+		
+		$pathinfo = pathinfo($asset->filename);
+		$resized_filename = $pathinfo['filename'];		
+		$resized_path = Kohana::config('admin/asset.upload_path').'/'.$resized_filename.$asset->extension;
+		
+		if ($width AND $height)
+		{
+			$resized_filename .= "_{$width}_{$height}_{$crop}";		
+			$resized_path = Kohana::config('admin/asset.upload_path').'/resized/'.$resized_filename.$asset->extension;
+		}
+		
+		if (!file_exists(DOCROOT.$resized_path))
+		{
+			Image::factory( $file )
+				->resize($width, $height)
+				->save($resized_path);
+		}
+			
+		$this->request->send_file($resized_path, FALSE, array('inline' => true));
+	}
+	
 	public function action_edit($id = 0)
 	{
 		$asset = ORM::factory('asset', (int) $id);
@@ -50,7 +83,6 @@ class Controller_Admin_Assets extends Controller_Admin_Base {
 		if (!$asset->loaded())
 		{
 			Message::set(MESSAGE::ERROR, __('Asset not found.'));
-
 			$this->response->redirect('admin');
 		} 
 		
@@ -64,8 +96,7 @@ class Controller_Admin_Assets extends Controller_Admin_Base {
 			
 		if (ORM::factory('asset')->update_admin($_POST))
 		{
-			Message::set(Message::SUCCESS, __('Asset successfully updated.'));
-			
+			Message::set(Message::SUCCESS, __('Asset successfully updated.'));			
 			$this->request->redirect($this->request->uri);
 		}
 		
@@ -75,8 +106,7 @@ class Controller_Admin_Assets extends Controller_Admin_Base {
 		}
 
 		// If POST is empty, then add the default data to POST
-		isset($default_data) AND $_POST = array_merge($_POST->as_array(), $default_data);
-		
+		isset($default_data) AND $_POST = array_merge($_POST->as_array(), $default_data);		
 	}
 
 } // End Controller_Admin_Assets
