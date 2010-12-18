408ac804dd82d1966e7a959d34d908895bb0df22
Richard Willis <willis.rh@gmail.com>
2010-12-18 15:38:52 +0000

    added admin config

---
+++ config/admin/media.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'styles' => array(
+		'media/css/base.css',
+		'modules/admin/media/js/jquery-ui/build/dist/jquery-ui-1.9pre/themes/base/jquery-ui.css',
+		'modules/admin/media/css/jquery.ui.theme.admin.css',
+		'modules/admin/media/css/admin.css'
+	),
+	'scripts' => array(
+		'media/js/jquery-1.4.4.min.js',
+		'modules/admin/media/js/jquery-ui/build/dist/jquery-ui-1.9pre/ui/jquery-ui.js',
+		'modules/admin/media/js/jquery.tree.js',
+		'modules/admin/media/js/require.js',
+		'modules/admin/media/js/admin.js'
+	),
+	'paths' => array(
+		'base' => 'admin',
+		'tinymce_skin' => 'modules/admin/media/js/tinymce/jscripts/tiny_mce/themes/advanced/skins/cirkuit/ui.css',
+		'tinymce' => 'modules/admin/media/js/tinymce/jscripts/tiny_mce/tiny_mce.js',
+		'tinymce_init' => 'admin/media/js/wysiwyg.init.js'
+	)		
+);
