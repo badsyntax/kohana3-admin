848594970c31813235934145785660c9e4d31006
Richard Willis <willis.rh@gmail.com>
2010-12-20 23:49:13 +0000

    better class layout, ajax methods and other adjustments

--- media/js/admin.js
+++ media/js/admin.js
@@ -7,43 +7,65 @@
 
 (function(window, $){
 
-	if (window.VEX) return;
+	if (window.Admin) return;
 
-	var VEX = {};
+	var Admin = {};
 	
-	VEX.util = VEX.controller = {};
+	Admin.util = Admin.controller = {};
 	
-	VEX.init = function(config){
+	Admin.init = function(config){
 		
 		this.config = config;
-	
-		this.util.ui('body');
+
+		var self = this;
 		
-		this.util.validate();
-			
-		this.bootstrap(config.route);
-	};
-	
-	VEX.bootstrap = function(route){
+
+		/*
+		 * PRIVATE METHODS
+		 */
 		
-		if (route.controller && this.controller[route.controller]) {
-			
-			if (this.controller[route.controller]['init']) {
-			
-				this.controller[route.controller].init();
-			}
-			
-			if (route.action && this.controller[route.controller][route.action]) {
-			
-				this.controller[route.controller][route.action]();
+		// build the ui, reset ajax actions, bind common page events
+		function setup(){
+
+			self.util.ajaxLoad(true);
+
+			self.util.ui('body');
+
+			self.util.validate();
+		}
+		
+		// execute a controller action		
+		function bootstrap(route){
+	
+			if (route.controller && self.controller[route.controller]) {
+
+				if (self.controller[route.controller]['init']) {
+
+					self.controller[route.controller]['init']();
+				}
+
+				if (route.action && self.controller[route.controller]['action_' + route.action]) {
+
+					self.controller[route.controller]['action_' + route.action]();
+				}
 			}
 		}
+		
+		/*
+		 * PRIVILEDGED METHODS
+		 */
+		
+		setup();
+		
+		// begin the routing
+		bootstrap(config.route);
 	};
 	
-	VEX.util.ui = function(selector){
+	Admin.util.ui = function(selector){
 		
 		var elem = $(selector);
 		
+		/* Messages */
 		elem.find('#messages').children().length 
 			&& $('#messages')
 				.bind('show.vex', function(){
@@ -51,22 +73,36 @@
 				})
 				.trigger('show.vex');
 
-		elem.find('select').selectmenu();
+		/* Selectmenu */
+		elem
+			.find('select')
+			.selectmenu();
 
-		elem.find('button.ui-button.save').button({
-			icons: {
-				primary: "ui-icon-disk"
-			}
-		});
-
-		elem.find('button.default').button();
-
-		elem.find('.tabs').tabs();
-
-		elem.find('.ui-tree ul:first').tree({
-				expanded: 'li:first'
+		/* Save Button */
+		elem
+			.find('button.ui-button.save')
+			.button({
+				icons: {
+					primary: "ui-icon-disk"
+				}
 			});
 
+		/* Default Button */
+		elem
+			.find('button.default')
+			.button();
+
+		/* Tabs */
+		elem
+			.find('.tabs')
+			.tabs();
+
+		/* Tree */
+		elem
+			.find('.ui-tree ul:first')
+			.tree();
+
+		/* Button Menu */
 		elem.find('.action-menu button')
 		.button({
 			icons: {
@@ -94,10 +130,12 @@
 
 			var btn = this;
 
-			$(this).trigger('mousedown.button').bind('mouseleave.admin.button', function(){
+			$(this)
+				.trigger('mousedown.button')
+				.bind('mouseleave.admin.button', function(){
 
-				$(this).addClass('ui-state-active');
-			});
+					$(this).addClass('ui-state-active');
+				});
 
 			var menu = $(this).next();
 
@@ -127,36 +165,47 @@
 		});
 	};
 	
-	VEX.util.trigger = function(scope, callback, arg){
+	Admin.util.trigger = function(scope, callback, arg){
 
 		var type = typeof callback;
+		
 		arg = arg || [];
+		
 	 	( type === 'function' ) && callback.apply( scope, arg );
 	};
 	
-	VEX.util.validate = function(){
+	Admin.util.ajaxLoad = function(finished){
+		
+		var method = finished ? 'fadeOut' : 'fadeIn';
+		
+		$('#ajax-loading img')[method]('fast');
+	};
+	
+	Admin.util.validate = function(){
 		
 		var form;
 		
 		function postSuccess(errors){
+			
+			Admin.util.ajaxLoad(true);
 
 			$('.form-error, .label-error').hide();
 
 			if (!errors.length && errors.length !== undefined) {
 
-				VEX.util.message('success', 'Successfully updated.');
+				Admin.util.message('success', 'Successfully updated.');
 
 			} else {
 				
-				VEX.util.message('error', 'Please correct the errors.');
+				Admin.util.message('error', 'Please correct the errors.');
 
 				$.each(errors, function(key, val){
 					
-					var 
-						id = $('[name="' + key + '"]').attr('id'),
+					var id = $('[name="' + key + '"]').attr('id'),
 						label = $('label[for="' + id + '"]');
 					
-					! label.find('.label-error').length && label.append('<span class="label-error"></span>');
+					!label.find('.label-error').length 
+						&& label.append('<span class="label-error"></span>');
 					
 					label.find('.label-error')
 						.hide()
@@ -169,13 +218,14 @@
 		$('.ajax-validate')
 			.submit(function(e){
 				
-				form = this;
-
 				e.preventDefault();
+				Admin.util.ajaxLoad();
+				form = this;
 
 				$.ajax({
 					type: 'POST',
-					url: this.action,
+					url: this.action + '?' + (new Date().getTime()),
+					cache: false,
 					data: $(this).serialize(),
 					dataType: 'json',
 					success: postSuccess
@@ -185,18 +235,17 @@
 			});
 	};
 	
-	VEX.util.message = function(type, msg){
+	Admin.util.message = function(type, msg){
 		
 		var elem = $('#messages').empty().hide(), 
 			item = '<li class="'+type+'">' + msg + '</li>';
 		
 		elem
 			.append('<ul>' + item + '</ul>')
-			.effect("highlight", {}, 600);
-		  
+			.fadeIn('fast').effect("highlight", {}, 800);			
 	}
 	
-	VEX.util.dialog = {
+	Admin.util.dialog = {
 		
 		confirm : function(msg, callback){
 			
@@ -206,50 +255,82 @@
 			}
 		}
 	};
-	
-	VEX.controller.pages = {
+
+	Admin.controller.pages = {
 		
 		init: function(){
 			
 		},
 		
-		index: function(){
-			$('#page-tree')
-				.html('Loading tree...')
-				.load(VEX.config.paths.base + '/pages/tree', function(){
+		action_index: function(){
+			
+			Admin.util.ajaxLoad();
+			
+			function load(){
+			
+				Admin.util.ajaxLoad(true);
 					
-					$(this)
-						.parent().ui().end()
-						.hide().fadeIn('fast');
-				});
+				$(this).parent().ui();
+				
+				$('#total-pages').html( $(this).find('a').length );
+				
+				$('fieldset.pages-list.last').removeClass('last');
+				
+				$('fieldset.pages-information').addClass('last').show();
+			}
+			
+			$('#page-tree').load(Admin.config.paths.base + '/pages/tree', load);
 		},
 		
-		add: function(){
-			// load in wysiwyg init
-			require([VEX.config.paths.tinymce, VEX.config.paths.tinymce_init], function() {
+		action_add: function(){
+			
+			// load and initiate wysiwyg
+			require([Admin.config.paths.tinymce, Admin.config.paths.tinymce_init], function() {
 			
 			});
 		},
 		
-		edit: function(){
-			// load in wysiwyg init
-			require([VEX.config.paths.tinymce, VEX.config.paths.tinymce_init], function() {
+		action_edit: function(){
+			
+			// load and initiate wysiwyg
+			require([Admin.config.paths.tinymce, Admin.config.paths.tinymce_init], function() {
 			
 			});
 		}
 	};
 	
-	VEX.controller.assets = {
+	Admin.controller.users = {
+	
+		init: function(){
+
+		},
+		
+		action_index: function(){
+			
+			Admin.util.ajaxLoad();
+			
+			function load(){
+			
+				Admin.util.ajaxLoad(true);
+					
+				$(this).parent().ui();				
+			}
+			
+			$('#page-tree').load(Admin.config.paths.base + '/users/tree', load);
+		}
+	};
+	
+	Admin.controller.assets = {
 		
 	};
 	
-	window.VEX = VEX;
+	window.Admin = Admin;
 	
 	$.fn.ui = function(){
 		
 		return this.each(function(){
 			
-			VEX.util.ui(this);
+			Admin.util.ui(this);
 		})
 	};
 	
