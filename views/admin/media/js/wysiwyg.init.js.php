d3a7b3d7f2b388b7ed270dae549322528964d248
Richard Willis <willis.rh@gmail.com>
2010-12-20 23:49:51 +0000

    no horizontal scrolling

--- views/admin/media/js/wysiwyg.init.js.php
+++ views/admin/media/js/wysiwyg.init.js.php
@@ -16,6 +16,7 @@
 		theme_advanced_buttons2 : "",
 		theme_advanced_buttons3 : "",
 		theme_advanced_buttons4 : "",
+		theme_advanced_resize_horizontal : false,
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_statusbar_location : "bottom",
