2a56ec37ae631004d413f5d14a0fee9f71f5fd8a
Richard Willis <willis.rh@gmail.com>
2010-12-23 00:32:41 +0000

    added groups index view

---
+++ views/admin/page/groups/index.php
@@ -0,0 +1,18 @@
+<div class="action-bar clear">
+	<div class="action-menu helper-right">
+		<button>Actions</button>
+		<ul>
+			<li><?php echo HTML::anchor('admin/groups/add/', __('Add group'))?></li>
+		</ul>
+	</div>
+
+	<h1>Groups</h1>
+</div>
+
+<fieldset class="last">
+	
+	<div id="page-tree" class="ui-tree">
+		loading tree...
+	</div>
+	
+</fieldset>
