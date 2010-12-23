988bbca33b9a03db724dac6023d3bf4d89044b90
Richard Willis <willis.rh@gmail.com>
2010-12-23 00:31:59 +0000

    added user tree views

--- views/admin/page/users/tree/item_open.php
+++ views/admin/page/users/tree/item_open.php
@@ -1,9 +1,2 @@
 <li>
-	<?php echo HTML::anchor('admin/groups/edit/'.$group->id, $group->name)?>
-	<?php if (count($group->users)){?>
-		<ul>
-		<?php foreach($group->users->find_all() as $user){?>
-			<li><?php echo HTML::anchor('admin/users/edit'.$user->id, $user->username)?></li>
-		<?php } ?>
-		</ul>
-	<?php }?>
+	<?php echo HTML::anchor('admin/groups/edit/'.$group->id, $group->name, array('class' => 'group'))?>
---
+++ views/admin/page/users/tree/users_item.php
@@ -0,0 +1,3 @@
+<li>
+	<?php echo HTML::anchor('admin/users/edit/'.$user->id, $user->username, array('class' => 'user'))?>
+</li>
