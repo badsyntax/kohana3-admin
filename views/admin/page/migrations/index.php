e86b1ac12a8b0d314b66464f7bb5e1fb94f79eb0
Richard Willis <willis.rh@gmail.com>
2010-12-29 00:21:55 +0000

    added migrations index view

---
+++ views/admin/page/migrations/index.php
@@ -0,0 +1,6 @@
+<h1>Migrations</h1>
+
+<ul>
+	<li><?php echo HTML::anchor('admin/migrations/save_mimetypes', 'Save mimetypes')?></li>
+	<li><?php echo HTML::anchor('admin/migrations/save_config', 'Save config')?></li>
+</ul>
