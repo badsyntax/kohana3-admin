d96a5c135dfc7edc152db68ad9effdedbb5a088a
Richard Willis <willis.rh@gmail.com>
2010-12-29 00:24:59 +0000

    added data id to anchor

--- views/admin/page/users/tree/item_open.php
+++ views/admin/page/users/tree/item_open.php
@@ -1,2 +1,5 @@
 <li>
-	<?php echo HTML::anchor('admin/groups/edit/'.$group->id, $group->name, array('class' => 'group'))?>
+	<?php echo HTML::anchor('admin/groups/edit/'.$group->id, $group->name, array(
+		'class' => 'page'.(in_array($group->id, $open_items) ? ' tree-open':''), 
+		'data-id' => $group->id
+	))?>
