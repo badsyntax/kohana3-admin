b44edc330010695b91845172d7581dc7c759199c
Richard Willis <willis.rh@gmail.com>
2010-12-29 00:22:47 +0000

    added asset information and image actions

--- views/admin/page/assets/edit.php
+++ views/admin/page/assets/edit.php
@@ -3,23 +3,47 @@
 		<button>Actions</button>
 		<ul>
 			<li><?php echo HTML::anchor('admin/assets/download/'.$asset->id, __('Download asset'))?></li>
+			<li><?php echo HTML::anchor('admin/assets/delete/'.$asset->id, __('Delete asset'))?></li>
 		</ul>
 	</div>
 	<h1>Edit asset</h1>
 </div>
 
-<?php echo Form::open(NULL, array('class' => 'assets-edit'))?>
+<?php echo Form::open(NULL, array('class' => 'assets-edit ajax-validate'))?>
 
 	<fieldset>
-		<legend>Edit asset</legend>
-		
+		<legend>Preview</legend>
 		<a href="<?php echo URL::site('admin/assets/get_asset/'.$asset->id)?>" class="thumb ui-lightbox">
 			<img src="<?php echo URL::site('admin/assets/get_asset/'.$asset->id)?>/300/300" />
 		</a>
+	</fieldset>
+
+	<fieldset>
+		<legend>Information</legend>
+		<strong>Uploaded by:</strong> <?php echo HTML::anchor('admin/users/view/'.$asset->user->id, $asset->user->username).' on '.$asset->date?> <br />
+		<strong>Mimetype:</strong> <?php echo $asset->mimetype->subtype.'/'.$asset->mimetype->type?> <br />
+		<strong>Filesize:</strong> <?php echo Text::bytes($asset->filesize)?><br />
+
+
+	</fieldset>
+	<fieldset>
+		<legend>Image actions</legend>
+	
+		<ul>
+			<li><?php echo HTML::anchor('admin/assets/rotate/'.$asset->id, 'Rotate 90deg')?></li>
+			<li><?php echo HTML::anchor('admin/assets/sharpen/'.$asset->id, 'Sharpen')?></li>
+			<li><?php echo HTML::anchor('admin/assets/flip_horizontal/'.$asset->id, 'Flip horizontal')?></li>
+			<li><?php echo HTML::anchor('admin/assets/flip_vertical/'.$asset->id, 'Flip vertical')?></li>						
+		</ul>
+	</fieldset>
+	
+	<fieldset class="last">
+		<legend>Edit asset</legend>
+		
 		<div class="field">
 			<?php echo
 				Form::label('filename', 'Filename', NULL, $errors).
-				Form::input('filenmae', $_POST['filename'], NULL, $errors)
+				Form::input('filename', $_POST['filename'], NULL, $errors)
 			?>
 		</div>
 		<div class="field">
