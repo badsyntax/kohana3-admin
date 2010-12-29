4d387a7e5174f2208c578570f07ebf9e0b74e0dd
Richard Willis <willis.rh@gmail.com>
2010-12-29 00:22:10 +0000

    added breadcrumbs fragment

---
+++ views/admin/page/fragments/breadcrumbs.php
@@ -0,0 +1,10 @@
+<ul id="breadcrumbs">
+	<?php foreach($pages as $c => $page){?>
+			<li>
+				<?php echo HTML::anchor($page['url'], $page['title'])?>
+				<?php if ($c < count($pages)-1){?>
+				&raquo;
+				<?php }?>
+			</li>
+	<?php }?>
+</ul>
