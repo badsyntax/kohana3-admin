0d8c6a078dee57ef02d422c50ed273b97105c6f5
Richard Willis <willis.rh@gmail.com>
2010-12-29 00:24:24 +0000

    added allowed uploaded types

--- views/admin/page/assets/upload.php
+++ views/admin/page/assets/upload.php
@@ -1,17 +1,23 @@
-<h1>Upload asset</h1>
+<h1>Upload assets</h1>
 
 <?php echo Form::open(NULL, array('enctype' => 'multipart/form-data'))?>
-
-	<fieldset>
+	<fieldset class="last">
 		<legend>Select file</legend>
 		
+		<p>Allowed upload types: <?php echo $allowed_upload_type?></p>
+		
+		<!--
 		<?php echo Form::label('asset', 'File', NULL, $errors)?>
+		-->
 		<div class="field">	
 			<?php echo Form::file('asset', NULL, $errors)?>
 		</div>
 		
-		<?php echo Form::button('save', 'Upload', array('type' => 'submit', 'class' => 'ui-button save'))?>
-		
+		<?php echo Form::button('save', 'Upload', array(
+			'type' => 'submit', 
+			'class' => 'ui-button save ui-helper-hiddens', 
+			'id' => 'upload-asset',
+			'style' => 'displsay:none'
+		))?>		
 	</fieldset>
-
 <?php echo Form::close()?>
