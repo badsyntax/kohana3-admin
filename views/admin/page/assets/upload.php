e2e10ecfc5ac7c06809e0fe2dbe4882a4d57d84d
Richard Willis <willis.rh@gmail.com>
2011-01-01 00:03:22 +0000

    added upload info

--- views/admin/page/assets/upload.php
+++ views/admin/page/assets/upload.php
@@ -4,20 +4,21 @@
 	<fieldset class="last">
 		<legend>Select file</legend>
 		
-		<p>Allowed upload types: <?php echo $allowed_upload_type?></p>
+		<p>Allowed types: <?php echo $allowed_upload_type?></p>
+		<p>Max uploads: <?php echo $max_file_uploads?></p>
 		
-		<!--
-		<?php echo Form::label('asset', 'File', NULL, $errors)?>
-		-->
+		
+		<?php echo Form::label('asset', '', NULL, $errors)?>	
+		
 		<div class="field">	
-			<?php echo Form::file('asset', NULL, $errors)?>
+			<?php echo Form::file($field_name.'[]', array('id' => '', 'class' => "multi", 'maxlength' => $max_upload), $errors)?>
 		</div>
 		
 		<?php echo Form::button('save', 'Upload', array(
 			'type' => 'submit', 
 			'class' => 'ui-button save ui-helper-hiddens', 
-			'id' => 'upload-asset',
-			'style' => 'displsay:none'
+			'id' => 'upload-asset'
 		))?>		
+		
 	</fieldset>
 <?php echo Form::close()?>
