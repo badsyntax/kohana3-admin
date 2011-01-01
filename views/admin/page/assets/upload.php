d30fbc9bddfd425a8d047cc7b2c22516fe52f4b3
Richard Willis <willis.rh@gmail.com>
2011-01-01 21:53:01 +0000

    added multiple upload errors

--- views/admin/page/assets/upload.php
+++ views/admin/page/assets/upload.php
@@ -7,11 +7,25 @@
 		<p>Allowed types: <?php echo $allowed_upload_type?></p>
 		<p>Max uploads: <?php echo $max_file_uploads?></p>
 		
-		
-		<?php echo Form::label('asset', '', NULL, $errors)?>	
-		
-		<div class="field">	
-			<?php echo Form::file($field_name.'[]', array('id' => '', 'class' => "multi", 'maxlength' => $max_upload), $errors)?>
+		<div class="field">
+			<?php if ($errors[$field_name]){?>
+				<strong>Errors:</strong><br />
+				<ul>
+					<?php foreach($errors[$field_name] as $field_errors){
+						foreach($field_errors as $error){?>
+							<li><?php echo $error?></li>
+						<?php }
+					}?>					
+				</ul>
+			<?php }?>
+			<div class="field">	
+				<?php echo Form::file($field_name.'[]', array(
+					'id' => '', 
+					'class' => 'multi',
+					'maxlength' => $max_file_uploads,
+					'accept' => preg_replace('/,\s*/', '|', $allowed_upload_type)
+				), $errors)?>
+			</div>
 		</div>
 		
 		<?php echo Form::button('save', 'Upload', array(
