6aa6c370bee4fc370f70288240c96a5ad2d308ae
Richard Willis <willis.rh@gmail.com>
2011-01-01 21:59:29 +0000

    updated paths

--- config/admin/media.php
+++ config/admin/media.php
@@ -12,12 +12,16 @@ return array(
 		'modules/admin/media/js/jquery-ui/build/dist/jquery-ui-1.9pre/ui/jquery-ui.js',
 		'modules/admin/media/js/jquery-tree/js/jquery.tree.js',
 		'modules/admin/media/js/require.js',
-		'modules/admin/media/js/admin.js'
+		'modules/admin/media/js/admin.js',
+		'modules/admin/media/js/admin.controllers.js',
+		'modules/admin/media/js/admin.models.js',
+		'modules/admin/media/js/admin.util.js',
 	),
 	'paths' => array(
 		'base' => 'admin',
 		'tinymce_skin' => 'modules/admin/media/js/tinymce/jscripts/tiny_mce/themes/advanced/skins/cirkuit/ui.css',
 		'tinymce' => 'modules/admin/media/js/tinymce/jscripts/tiny_mce/tiny_mce.js',
+		'tinymce_popup' => 'modules/admin/media/js/tinymce/jscripts/tiny_mce/tiny_mce_popup.js',
 		'tinymce_init' => 'admin/media/js/wysiwyg.init.js'
 	)		
 );
