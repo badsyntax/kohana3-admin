d25cac80aec1a5371a1e4b80597e5402bd419222
Richard Willis <willis.rh@gmail.com>
2011-01-01 00:02:13 +0000

    split the index view into browse and upload

--- views/admin/page/assets/index.php
+++ views/admin/page/assets/index.php
@@ -1,14 +1,28 @@
-<div class="action-bar clear">
+
+<div class="tabs">
 	
-	<div class="action-menu helper-right">
-		<button>Actions</button>
-		<ul>
-			<li><?php echo HTML::anchor('admin/assets/upload', __('Upload assets'))?></li>
-			<li><?php echo HTML::anchor('admin/assets/delete', __('Delete assets'), array('id' => 'delete-assets'))?></li>
-		</ul>
+
+	<ul>
+		<li><a href="#tabs-1">Thumbails</a></li>
+		<li><a href="#tabs-2">Details list</a></li>
+
+	</ul>
+	
+	<div class="action-bar clear" style="position: absolute;
+    right: 0.4em;
+    top: 0.4em;">
+
+		<div class="action-menu helper-right">
+			<button>Actions</button>
+			<ul>
+				<li><?php echo HTML::anchor('admin/assets/upload', __('Upload assets'))?></li>
+				<li><?php echo HTML::anchor('admin/assets/delete', __('Delete assets'), array('id' => 'delete-assets'))?></li>
+			</ul>
+		</div>
+
 	</div>
+	<div id="tabs-1">
 	<h1>Assets</h1>
-</div>
 
 <form id="assets-list">
 <fieldset>	
@@ -18,7 +32,7 @@
 			<li>
 				<input type="checkbox" class="checkbox" name="asset-<?php echo $asset->id?>" value="<?php echo $asset->id?>" id="asset-<?php echo $asset->id?>" />
 				<a href="<?php echo URL::site('admin/assets/edit/'.$asset->id)?>">
-					<img src="<?php echo URL::site('admin/assets/get_asset/'.$asset->id.'/100/100/1')?>" width="100" />
+					<img src="<?php echo URL::site($asset->image_url(100, 100, TRUE))?>" />
 				</a>
 			</li>
 		<?php }?>
@@ -30,6 +44,15 @@
 <fieldset class="last">
 	<div id="page-links">
 		<div style="float:right"><?php echo $page_links?></div>
-		Showing <?php echo $assets->count()?> of <?php echo $total?> assets
+		Showing <?php echo $assets->count()?> of <?php echo $total?> assets |
+		<a id="select-all" href="#select-all">Select all</a> |
+		<a id="select-none" href="#select-none">Select none</a>
 	</div>
 </fieldset>
+
+</div>
+<div id="tabs-2">
+	<p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+</div>
+
+</div>
