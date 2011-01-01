6e780b05414b4b75976024c33cd68437f47a7201
Richard Willis <willis.rh@gmail.com>
2011-01-01 00:08:22 +0000

    added publishing section

--- views/admin/page/pages/add.php
+++ views/admin/page/pages/add.php
@@ -1,21 +1,22 @@
 <?php echo Form::open(NULL, array('class' => 'ajax-validate'))?>
+
 	<fieldset>
 		<legend>Metadata</legend>
 		<div class="field">
 			<?php echo 
-				Form::label('title', __('Title'), NULL, $errors),
+				Form::label('title', __('Title'), NULL, $errors).
 				Form::input('title', $_POST['title'], NULL, $errors)
 			?>
 		</div>
 		<div class="field">
 			<?php echo 
-				Form::label('uri', __('URI'), NULL, $errors),
+				Form::label('uri', __('URI'), NULL, $errors).
 				Form::input('uri', $_POST['uri'], NULL, $errors)
 			?>
 		</div>
 		<div class="field">
 			<?php echo 
-				Form::label('description', __('Description'), NULL, $errors),
+				Form::label('description', __('Description'), NULL, $errors).
 				Form::input('description', $_POST['description'], NULL, $errors)
 			?>
 		</div>
@@ -25,22 +26,43 @@
 		<legend>Categorize</legend>
 		<div class="field">
 			<?php echo 
-				Form::label('parent_id', __('Parent page'), NULL, $errors),
+				Form::label('parent_id', __('Parent page'), NULL, $errors).
 				Form::select('parent_id', $pages, ($_POST['parent_id'] ? $_POST['parent_id'] : $parent_id), NULL, $errors)
+			?>
+		</div>
+		
+	</fieldset>
+	
+	<fieldset>
+		<legend>Publishing</legend>
+		<div class="field">
+			<?php echo 
+				Form::label('visible_from', __('Visible from'), NULL, $errors).
+				Form::input('visible_from', $_POST['visible_from'], array('class' => 'datepicker'), $errors)
+			?>
+		</div>
+		<div class="field">
+			<?php echo 
+				Form::label('visible_to', __('Visible to'), NULL, $errors).
+				Form::input('visible_to', $_POST['visible_to'], array('class' => 'datepicker'), $errors)
 			?>
 		</div>
 	</fieldset>
 
-	<fieldset class="last">
+	<fieldset>
 		<legend>Content</legend>
 		<div class="field">
 			<?php echo 
 				Form::label('body', __('Body content'), NULL, $errors),
 				Form::textarea('body', $_POST['body'], array('class' => 'wysiwyg'), TRUE, $errors)
 			?>
-		</div>
-		
-		<?php echo Form::button('save', 'Save', array('type' => 'submit', 'class' => 'ui-button save'))?>
-	
+		</div>		
 	</fieldset>
+	<?php echo Form::button('save', 'Save', array('type' => 'submit', 'class' => 'ui-button save'))?>
+	
+	<div class="helper-right">
+	<?php echo Form::button('preview', 'Preview', array('type' => 'submit', 'class' => 'ui-button save'))?>	
+	<?php echo Form::button('publish', 'Publish', array('type' => 'submit', 'class' => 'ui-button save'))?>
+	</div>
+	
 <?php echo Form::close()?>
