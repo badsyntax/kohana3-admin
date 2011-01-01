be763e2521e4450497cd4ddf8ea2961a29834475
Richard Willis <willis.rh@gmail.com>
2011-01-01 00:08:43 +0000

    open classes on item

--- views/admin/page/pages/tree/item_open.php
+++ views/admin/page/pages/tree/item_open.php
@@ -1,2 +1,5 @@
 <li>
-	<?php echo HTML::anchor('admin/pages/edit/'.$page->id, $page->title, array('class' => 'page'))?>
+	<?php echo HTML::anchor('admin/pages/edit/'.$page->id, $page->title, array(
+		'class' => 'page'.(in_array($page->id, $open_items) ? ' tree-open':''), 
+		'data-id' => $page->id
+	))?>
