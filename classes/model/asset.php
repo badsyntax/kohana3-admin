88c37ac4dd396246ed734a6b66defb2dc923633b
Richard Willis <willis.rh@gmail.com>
2011-01-01 00:07:17 +0000

    reworked the upload method

--- classes/model/asset.php
+++ classes/model/asset.php
@@ -2,50 +2,65 @@
 
 class Model_Asset extends Model_Base_Asset {
 	
-	public function admin_upload($name, $upload_path, & $post, & $files)
+	public function upload(& $file = array(), $field_name = 'asset')
 	{
-		$post = Validate::factory($post);
+		$file_data = $file;
+		$filename = $file[$field_name]['name'];
 		
-		$files = Validate::factory($files)
-			->rules($name, $this->_rules['upload'])
-			->rules('upload', array('Upload::type' => explode(',', Kohana::config('asset.allowed_upload_type'))));
+		// Get the validation rules
+		$rules = $this->_rules['upload'];
+		
+		// Add allowed upload types to validation
+		$rules['Upload::type'] = array(explode(',', Kohana::config('asset.allowed_upload_type')));
+
+		// Add file extension to file array (which we'll be validating against the available mimetypes)
+		$file['extension'] = trim(strrchr($filename, '.'), '.');
+
+		// Add validation rules
+		$file = Validate::factory($file)
+			->rules($field_name, $rules);
 						
-		if (!$files->check())
-			return FALSE;
-		
-		$assets = array();
-		
-		foreach($files as $file)
-		{
-			$fileext = trim(strrchr($file['name'], '.'), '.');
-		
-			$filename = $file['name'];
-			
-			// Try find a matching mimetype
-			$mimetype = ORM::factory('mimetype')->where('extension', '=', $fileext)->find();
-			
-			if (!$mimetype->loaded()) continue;
-		
-			try {
-				
-				$filename = Upload::save($file, $filename, DOCROOT.$upload_path);
-			}
-			catch(Exception $e)
-			{
-				continue;
-			}
-			
-			$asset = ORM::factory('asset');
-			$asset->user_id = Auth::instance()->get_user()->id;
-			$asset->mimetype_id = $mimetype->id;
-			$asset->filename = basename($filename);
-			$asset->filesize = (int) $file['size'];
-			$asset->save();
-			
-			$assets[] = $asset->id;
+		// Add validation callbacks			
+		foreach($this->_callbacks['extension'] as $callback)
+        {
+			$file->callback('extension', array($this, $callback));
 		}
 		
-		return $assets;
+		// Validate the data
+		if (!$file->check())
+		{	
+			return $this;
+		}
+
+		// Try move the asset to the specified upload path
+		try {	
+
+			$filename = Upload::save($file_data[$field_name], $filename, DOCROOT.Kohana::config('admin/asset.upload_path'));
+		}
+		catch(Exception $e)
+		{
+			return $this;
+		}
+		
+		// Save the file data
+		$data = array(
+			'user_id' => Auth::instance()->get_user()->id,
+			'mimetype_id' => $file['mimetype_id'],
+			'filename' => basename($filename),
+			'filesize' => (int) $file_data[$field_name]['size']
+		);		
+		$this->values($data);
+		$this->save();
+		
+		// Create a new filename with id prefixed
+		$new_filename = str_replace($this->filename, $this->id.'_'.$this->filename, $filename);
+		$this->filename = basename($new_filename);
+		$this->save();
+
+		// Move the file to the new filename path
+		rename($filename, $new_filename);
+
+		return $this;
 	}
 	
 	public function admin_update(& $data)
