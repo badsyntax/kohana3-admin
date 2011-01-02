de4d84eb47ace3eb169625c9dc77c4306abe7e5d
Richard Willis <willis.rh@gmail.com>
2011-01-02 21:31:09 +0000

    added asset info and insert slitbutton

--- views/admin/page/assets_popup/view.php
+++ views/admin/page/assets_popup/view.php
@@ -1,15 +1,34 @@
 <fieldset class="clear">
 	<legend>Preview</legend>
 	<input type="hidden" id="insert_path" value="<?php echo URL::site($asset->image_url(300, 300))?>" />
-	<a href="<?php echo URL::site('admin/assets/get_asset/'.$asset->id)?>" class="thumb ui-lightbox" style="float:left;margin-right:10px">
+	<div style="float:left;margin-right:10px">
 		<img src="<?php echo URL::site($asset->image_url(200, 300))?>" style="border:1px solid #ccc;padding:3px;"/>
-	</a>
+	</div>
 	<div>
 		<p>
-		<strong>Mimetype:</strong> <?php echo $asset->mimetype->subtype.'/'.$asset->mimetype->type?> 
-		</p>			
+			<strong>Mimetype:</strong> <?php echo $asset->mimetype->subtype.'/'.$asset->mimetype->type?> 
+		</p>
+		<?php if ($asset->is_image()){?>
 		<p>
-		<?php echo Form::button('insert', 'Insert Asset', array('type' => 'button', 'id' => 'insert-asset', 'class' => 'ui-button save'))?>
+			<strong>Width:</strong> <?php echo $asset->width?> x <?php echo $asset->height?>
+		</p>
+		<?php }?>
+		<p>
+			<strong>Filesize:</strong> <?php echo Text::bytes($asset->filesize)?>
+		</p>
+		<p>
+			<?php if ($asset->is_image()){?>
+				<div class="ui-buttonset helper-left" style="margin-right:10px">
+					<?php echo Form::button('insert', 'Insert Asset', array('type' => 'button', 'id' => 'insert-'.$asset->id, 'class' => 'insert-asset ui-button save'))?>
+					<button id="action-<?php echo $asset->id?>">Select an action</button>
+				</div>
+				<ul>
+					<li><?php echo HTML::anchor('admin/assets/popup/resize/'.$asset->id, 'Insert resized image', array('class' => 'resize-insert'))?></li>
+				</ul>
+			<?php } else {?>
+				<?php echo Form::button('insert', 'Insert Asset', array('type' => 'button', 'id' => 'insert-'.$asset->id, 'class' => 'insert-asset ui-button save'))?>
+			<?php }?>
+			<?php echo HTML::anchor('/admin/assets/popup/download/'.$asset->id, 'Download', array('class' => 'ui-button default'))?>
 		</p>
 	</div>
 </fieldset>
