408e0cb2d86c6965cde31a8c07577720861bfda9
Richard Willis <willis.rh@gmail.com>
2011-01-02 21:31:29 +0000

    cleaning up the nav gen code

--- views/admin/page/fragments/nav.php
+++ views/admin/page/fragments/nav.php
@@ -1,20 +1,20 @@
 <nav class="second-level">
 	
-		<ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
+	<ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
+		
 		<?php
-			$nav = Kohana::config('admin/nav');
-			$uri_segments = explode('/', Request::instance()->uri);
+		$nav = Kohana::config('admin/nav');
+		$uri_segments = explode('/', Request::instance()->uri);
 
-			foreach($nav['links'] as $url => $text) {
-				$classes = ($url === Request::instance()->uri OR $url === $uri_segments[0].'/'.@$uri_segments[1])
-					? ' ui-tabs-selected ui-state-active'
-					: '';
-				echo '<li class="ui-state-default ui-corner-top'.$classes.'">'.HTML::anchor($url, $text).'</li>';
-			}
-		?>
+		foreach($nav['links'] as $url => $text) {				
+			$classes = ($url === Request::instance()->uri OR $url === $uri_segments[0].'/'.@$uri_segments[1])
+				? ' ui-tabs-selected ui-state-active'
+				: '';?>
+			<li class="ui-state-default ui-corner-top<?php echo $classes?>"><?php echo HTML::anchor($url, $text)?></li>
+		<?php }?>
+		
 		<li class="ui-state-default ui-corner-top ui-helper-right">
 			<?php echo HTML::anchor('admin/signout', 'Sign out')?>
 		</li>
-		</ul>
-
+	</ul>
 </nav>
