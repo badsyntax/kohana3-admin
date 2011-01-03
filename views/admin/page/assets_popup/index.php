f9adc27932a3d19e5c1ac57112d55a5a5132e1de
Richard Willis <willis.rh@gmail.com>
2011-01-03 15:25:19 +0000

    added search tab html (commented out for now)

--- views/admin/page/assets_popup/index.php
+++ views/admin/page/assets_popup/index.php
@@ -3,17 +3,36 @@
 	<ul>
 		<li><a href="#browse">Browse</a></li>
 		<li><a href="#upload">Upload</a></li>
+		<!--<li><a href="#search">Search</a></li>-->
 	</ul>
 	<div id="browse">
 		<?php echo $browse_html?>
-
 	</div>	
 	<div id="upload">
 		<?php echo $upload_html?>
-	</div>	
+	</div>
+	<!--
+	<div id="search">
+		<?php echo Form::open('admin/assets/popup#browse')?>
+
+			<fieldset class="last">
+
+				<div class="field">	
+					<?php echo 
+						Form::label('search-query', __('Query'), NULL, $errors),
+						Form::input('search-query', $_POST['search-query'], NULL, $errors)
+					?>
+				</div>
+				<?php echo Form::button('search-submit', 'Search', array('type' => 'submit', 'class' => 'ui-button default'))?>	
+
+			</fieldset>
+
+		<?php echo Form::close()?>
+	</div>
+	-->
 </div>
 
-<fieldset id="page-links" class="last ui-widget ui-helper-hidden">
+<fieldset id="page-links" class="last ui-widget">
 	<div style="float:right"><?php echo $page_links?></div>
 	Showing <?php echo $assets->count()?> of <?php echo $total?> assets
 </fieldset>
