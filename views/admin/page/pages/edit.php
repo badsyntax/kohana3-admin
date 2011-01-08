4d27a2bb167eb8338118c6297e42d752a24aa78f
Richard Willis <willis.rh@gmail.com>
2011-01-08 21:11:07 +0000

    updated to reflect add form fields

--- views/admin/page/pages/edit.php
+++ views/admin/page/pages/edit.php
@@ -18,40 +18,82 @@
 </div>
 
 <?php echo Form::open(NULL, array('class' => 'ajax-validate'))?>
+
 	<fieldset>
-		
+		<legend>Metadata</legend>
 		<div class="field">
 			<?php echo 
-				Form::label('parent_id', __('Parent page'), NULL, $errors),
-				Form::select('parent_id', $pages, $_POST['parent_id'], NULL, $errors)
-			?>
-		</div>
-		<div class="field">
-			<?php echo 
-				Form::label('title', __('Title'), NULL, $errors),
+				Form::label('title', __('Title'), NULL, $errors).
 				Form::input('title', $_POST['title'], NULL, $errors)
 			?>
 		</div>
 		<div class="field">
 			<?php echo 
-				Form::label('uri', __('URI'), NULL, $errors),
+				Form::label('uri', __('URI'), NULL, $errors).
 				Form::input('uri', $_POST['uri'], NULL, $errors)
 			?>
 		</div>
 		<div class="field">
 			<?php echo 
-				Form::label('description', __('Description'), NULL, $errors),
+				Form::label('description', __('Description'), NULL, $errors).
 				Form::input('description', $_POST['description'], NULL, $errors)
 			?>
 		</div>
+	</fieldset>
+	
+	<fieldset>
+		<legend>Categorize</legend>
 		<div class="field">
 			<?php echo 
-				Form::label('body', __('Content'), NULL, $errors),
-				Form::textarea('body', $_POST['body'], NULL, TRUE, $errors)
+				Form::label('parent_id', __('Parent page'), NULL, $errors, 'admin/messages/label_error').
+				Form::select('parent_id', $pages, ($_POST['parent_id'] ? $_POST['parent_id'] : $parent_id), NULL, $errors)
 			?>
 		</div>
-
-		<?php echo Form::button('save', 'Save', array('type' => 'submit', 'class' => 'ui-button save'))?>
 		
 	</fieldset>
+	
+	<fieldset>
+		<legend>Publishing</legend>
+		<div class="field datepicker-wrapper clear">
+			<div class="clear">
+				<?php echo 
+					Form::label('visible_from', __('Visible from'), NULL, $errors);
+				?>
+			</div>
+			<div>
+				<?php echo
+					Form::input('visible_from', $_POST['visible_from'], array('class' => 'datepicker'), $errors);
+				?>
+			</div>
+		</div>
+		<div class="field datepicker-wrapper clear">
+			<div class="clear">
+				<?php echo 
+					Form::label('visible_to', __('Visible to'), NULL, $errors)
+				?>
+			</div>
+			<div>
+				<?php echo 
+					Form::input('visible_to', $_POST['visible_to'], array('class' => 'datepicker'), $errors);
+				?>
+			</div>
+		</div>
+	</fieldset>
+
+	<fieldset>
+		<legend>Content</legend>
+		<div class="field">
+			<?php echo 
+				Form::label('body', __('Body content'), NULL, $errors),
+				Form::textarea('body', $_POST['body'], array('class' => 'wysiwyg'), TRUE, $errors)
+			?>
+		</div>		
+	</fieldset>
+	<?php echo Form::button('save', 'Save', array('type' => 'submit', 'class' => 'ui-button save'))?>
+	
+	<div class="helper-right">
+	<?php echo Form::button('preview', 'Preview', array('type' => 'submit', 'class' => 'ui-button save'))?>	
+	<?php echo Form::button('publish', 'Publish', array('type' => 'submit', 'class' => 'ui-button save'))?>
+	</div>
+	
 <?php echo Form::close()?>
