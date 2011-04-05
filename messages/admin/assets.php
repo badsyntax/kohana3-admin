6d80ba2252c5ed027f3bead526bc9ba76878dca1
badsyntax <willis.rh@gmail.com>
2011-04-05 20:21:39 +0100

    added form messages

---
+++ messages/admin/assets.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+ 
+return array(
+	'asset' => array(
+		'Upload::not_empty' => __('The filename cannot be empty.'),
+	),
+	'extension' => array(
+		'mimetype_not_allowed' => __('Invalid filetype.')
+	)
+);
--- messages/admin/pages.php
+++ messages/admin/pages.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
  
 return array(
-    'parent_id.root_reparent' => __('the page cannot be saved at root'),
+	'parent_id.root_reparent' => __('the page cannot be saved at root'),
 );
