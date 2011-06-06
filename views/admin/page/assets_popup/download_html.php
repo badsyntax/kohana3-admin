543e96f2991a59de3d62582e2638700f6e303626
badsyntax <willis.rh@gmail.com>
2011-06-06 21:08:39 +0100

    removed asset manager (moved into a module)

--- views/admin/page/assets_popup/download_html.php
+++
@@ -1,4 +0,0 @@
-<span class="asset-download asset-<?php echo $asset->mimetype->type?>">
-	<a href="<?php echo $asset->url(TRUE)?>"><?php echo $asset->friendly_filename?></a> 
-	(<?php echo Text::bytes($asset->filesize)?>)
-</span>
